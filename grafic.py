6fbdc021ed59fd37c9dc1148b29507261b5b9180
jfarfanl <98135049+jfarfanl@users.noreply.github.com>
2023-12-02 23:13:22 -0500

    Update grafic.py

--- grafic.py
+++ grafic.py
@@ -1,43 +1,23 @@
 from flask import Flask, render_template
-import matplotlib.pyplot as plt
-import mysql.connector
+import requests
 
 app = Flask(__name__)
 
-# Conexión a la base de datos MySQL
-conn = mysql.connector.connect(
-    host='https://databases-auth.000webhost.com',
-    user='id20857986_root',
-    password='ddmnhn_4768252I',
-    database='id20857986_medidor1'
-)
-
-
 @app.route('/')
-def index():
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, RMSv1 FROM rms ORDER BY id DESC LIMIT 10")
-    data = cursor.fetchall()
+def mostrar_archivo():
+    # URL del archivo de texto en el servidor web
+    url_archivo = 'https://unlike-travel.000webhostapp.com/medidor/freqss.txt'  # Reemplaza esta URL por la dirección del archivo que desees leer
 
-    # Procesamiento de los datos para graficar
-    x = [row[0] for row in data]
-    y = [row[1] for row in data]
-
-    # Crear el gráfico de barras con Matplotlib
-    plt.figure(figsize=(10, 6))
-    plt.bar(x, y, color='blue')
-    plt.title('Últimos datos en Gráfico de Barras')
-    plt.xlabel('Eje X')
-    plt.ylabel('Eje Y')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-
-    # Guardar la imagen del gráfico en un archivo
-    nombre_archivo = 'static/grafico_barras.png'  # Ruta donde se guardará la imagen
-    plt.savefig(nombre_archivo)
-
-    cursor.close()
-    return render_template('index.html', grafico=nombre_archivo)
+    # Intentar obtener el contenido del archivo de texto desde la URL
+    try:
+        response = requests.get(url_archivo)
+        if response.status_code == 200:
+            contenido = response.text
+            return render_template('mostrar_archivo.html', contenido=contenido)
+        else:
+            return "Error al obtener el archivo: Código de estado " + str(response.status_code)
+    except requests.RequestException as e:
+        return "Error de conexión: " + str(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
