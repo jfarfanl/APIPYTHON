9abfd03b3ab2d239c95f2d80041665fc024b7813
jfarfanl <98135049+jfarfanl@users.noreply.github.com>
2023-12-02 22:58:38 -0500

    Update grafic.py

--- grafic.py
+++ grafic.py
@@ -1,8 +1,43 @@
-# save this as app.py
-from flask import Flask
+from flask import Flask, render_template
+import matplotlib.pyplot as plt
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+# Conexión a la base de datos MySQL
+conn = mysql.connector.connect(
+    host='https://databases-auth.000webhost.com',
+    user='id20857986_root',
+    password='ddmnhn_4768252I',
+    database='id20857986_medidor1'
+)
+
+
+@app.route('/')
+def index():
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, RMSv1 FROM rms ORDER BY id DESC LIMIT 10")
+    data = cursor.fetchall()
+
+    # Procesamiento de los datos para graficar
+    x = [row[0] for row in data]
+    y = [row[1] for row in data]
+
+    # Crear el gráfico de barras con Matplotlib
+    plt.figure(figsize=(10, 6))
+    plt.bar(x, y, color='blue')
+    plt.title('Últimos datos en Gráfico de Barras')
+    plt.xlabel('Eje X')
+    plt.ylabel('Eje Y')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    # Guardar la imagen del gráfico en un archivo
+    nombre_archivo = 'static/grafico_barras.png'  # Ruta donde se guardará la imagen
+    plt.savefig(nombre_archivo)
+
+    cursor.close()
+    return render_template('index.html', grafico=nombre_archivo)
+
+if __name__ == '__main__':
+    app.run(debug=True)
